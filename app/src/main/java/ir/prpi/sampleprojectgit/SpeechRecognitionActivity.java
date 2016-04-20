85086952b6c76497e922a391bdf668b2172bb3bf
Mohammad Nourinick <CEO@prpi.ir>
2016-04-20 16:04:33 +0430

    Init commit

---
+++ app/src/main/java/ir/prpi/sampleprojectgit/SpeechRecognitionActivity.java
@@ -0,0 +1,13 @@
+package ir.prpi.sampleprojectgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_recognition);
+    }
+}
